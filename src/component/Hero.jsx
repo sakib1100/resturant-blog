4c122dad8f158c59e121400c662af7c15325f520
sakib1100 <mostafmohsin783@gmail.com>
2022-12-06 17:47:23 +0600

    full update

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
             <div className="relative max-h-[500px]">
                 {/* overlay */}
                 <div className="absolute w-full h-full max-h-[500px] text-gray-200 bg-black/40 flex flex-col justify-center">
-                    <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">The <span className="text-orange-500">Best</span></h1>
+                    <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">Our <span className="text-orange-500">Best</span></h1>
                     <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold"><span className="text-orange-500">Food</span> Delivered</h1>
                 </div>
 
